dfe3049cd371d90e687fd32e09a515779f59b29e
taka438 <70242185+taka438@users.noreply.github.com>
2020-12-01 10:01:26 +0900

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,79 @@
+$(function() {
+  // ヘッダーfixed
+  $(window).scroll(function() {
+    if ($(window).scrollTop() !== 0) {
+      $('body').addClass('is-fixed');
+      $('#page-home .logo_kadan').attr('src', './kadan-image/kadan-room/sub-header-logo.png');
+    } else {
+      $('body').removeClass('is-fixed');
+      $('#page-home .logo_kadan').attr('src', './kadan-image/kadan-main/top-header-logo.png');
+    }
+  });
+
+  // ハンバーガーメニュー
+  $('.hb__btn').click(function() {
+    if ($('body').hasClass('hb_is-open')) {
+      $('body').removeClass('hb_is-open');
+      $('.hb__bg').fadeOut();
+    } else {
+      $('body').addClass('hb_is-open');
+      $('.hb__bg').fadeIn();
+    }
+  });
+  $('.hb__bg, .sp-rv__btn').click(function() {
+    $('body').removeClass('hb_is-open');
+    $('.hb__bg').fadeOut();
+  });
+  $('.sp-nav-global__item a').click(function() {
+    $('.hb__btn').trigger('click');
+  });
+
+  // スライドショー
+  var count = $('#slide li').length;
+  var current = 1;
+  var next = 2;
+  var interval = 4000;
+  var duration = 500;
+  var timer;
+
+  $('#slide li:not(:first-child)').hide();
+  timer = setInterval(slideTimer, interval);
+
+  function slideTimer() {
+    $('#slide li:nth-child(' + current + ')').fadeOut(duration);
+    $('#slide li:nth-child(' + next + ')').fadeIn(duration);
+    current = next;
+    next = ++next;
+    if (next > count) {
+      next = 1;
+    }
+  }
+
+  // タブ
+  $('.tab').click(function() {
+    $('#contents .contents-media').hide();
+    $(".contents > ." + $(this).attr('id')).show();
+    $('.current').removeClass('current');
+    $(this).addClass('current');
+    return false;
+  });
+
+  // 日付カレンダー
+  flatpickr("#flatpickr", {
+    locale:"ja",
+    enableTime: true,
+    dateFormat: "Y-m-d",
+    minDate:"today",
+    mode: "multiple",
+  });
+
+  // モーダル
+  $('.rv__btn, .sp-rv__btn').click(function() {
+    $('.modal__bg, .modal-contents').fadeIn();
+    return false;
+  });
+  $('.modal__btn, .modal__bg').click(function() {
+    $('.modal__bg, .modal-contents').fadeOut();
+  });
+
+});
